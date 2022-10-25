8cdacb2678c915a10a2e3db82a7255acbee5690f
AnaCascante <ana.cascante@stud.noroff.no>
2022-10-25 20:00:13 +0200

    1rst try

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// question 1 
+
+function receivingFunction () {
+    if (receivingFunction = function() {
+        console.log (receivingFunction);
+        })
+}
+
+function callbackFunction (){
+    console.log ("I am a call back function");
+}
+
+receivingFunction = callbackFunction; 
+
+
+// Question 2
+
+
+const prizes = ["Bag of bananas", "Box of frogs", "Car of clowns"];
+
+prizes.forEach(element => {
+    console.log (element);
+});
+
+
+// Question 3
+
+const executing (); 
+
+setTimeout((executing) => {
+    console.log (`I waited 5 secons before executing`)
+}, timeout 5000 );
+
+// Question 4
+
+Write code that logs the numbers 1 to 4 every 1.5 seconds and then stops.
+
+const numbers
+let counter = 0 
+
+function createHTML () { 
+    counter ++; 
+    if (counter = 4){ clearInterval (interval)}
+}
+
+const interval = setInterval (createHTML, 1500)
